c092c933b00eda5725c0c5ceee8427adfeba1bbd
santiagomac <smazuerac@unac.edu.co>
2024-07-28 21:08:17 -0500

    fix the responsive of form

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -34,7 +34,7 @@ export const Modal = () => {
         aria-hidden="true"
       ></div>
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="flex flex-col min-h-full justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg flex flex-col items-center">
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
@@ -44,7 +44,7 @@ export const Modal = () => {
                 />
               </div>
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 w-1/2 flex items-center justify-between">
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 w-full flex items-center gap-x-4 justify-between md:w-1/2">
               <button
                 type="button"
                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
@@ -54,7 +54,7 @@ export const Modal = () => {
               </button>
               <button
                 type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 onClick={changeModalState}
               >
                 Cancel
