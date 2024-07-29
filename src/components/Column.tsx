5b74656016101356deacb53483a7431b46cfdacf
santiagomac <smazuerac@unac.edu.co>
2024-07-28 21:49:50 -0500

    fix the dropable height

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -9,29 +9,29 @@ interface Props {
 
 export const Column = ({ column, tasks = [] }: Props) => {
   return (
-    <div
-      className={`bg-white dark:bg-slate-950 dark:border-slate-800 dark:border-2 w-full p-5 rounded-lg shadow-lg md:h-2/3 md:w-1/3 ${
-        tasks.length === 0 ? "h-[100px]" : "h-full"
-      }`}
-    >
-      <h2 className="font-bold text-2xl mb-4">{column.title}</h2>
-      <Droppable droppableId={column.id}>
-        {(provided, snapshot) => (
-          <div
-            className={`flex flex-col gap-5 ${
-              snapshot.isDraggingOver &&
-              "bg-gray-200 dark:bg-slate-400 p-3 rounded"
-            }`}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {tasks.map((task, index) => {
-              return <Task key={task.id} task={task} index={index} />;
-            })}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </div>
+    <>
+      <div
+        className={`bg-white h-full dark:bg-slate-950 dark:border-slate-800 dark:border-2 w-full p-5 rounded-lg shadow-lg md:h-2/3 md:w-1/3`}
+      >
+        <h2 className="font-bold text-2xl mb-2">{column.title}</h2>
+        <Droppable droppableId={column.id}>
+          {(provided, snapshot) => (
+            <div
+              className={`flex flex-col h-[90%] overflow-hidden gap-y-5 ${
+                snapshot.isDraggingOver &&
+                "bg-gray-200 dark:bg-slate-400 rounded p-1"
+              }`}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {tasks.map((task, index) => {
+                return <Task key={task.id} task={task} index={index} />;
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </>
   );
 };
